68085f7befcbdbdbb8f6d9b7e14af41f2e984466
yuuu085 <y.r.0850@gmail.com>
2025-11-09 02:10:41 +0900

    ファイルをアップロード
    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>kadai_007</title>
+</head>
+
+<body>
+   <p>
+     <?php
+     $vegetable_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+     print_r($vegetable_data);
+     ?>
+   </p>
+</body>
+
+</html>
