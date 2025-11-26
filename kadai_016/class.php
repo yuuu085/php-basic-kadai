44520c12966b87e8be81d26d5ffd1e78c53c850b
yuuu085 <y.r.0850@gmail.com>
2025-11-27 03:30:22 +0900

    ファイルをアップロード
    kadai_016をアップロード

---
+++ kadai_016/class.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>kadai_016</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       class Food {
+           private $name;
+           private $price;
+
+           public function __construct(string $name, int $price) {
+                $this->name = $name;
+                $this->price = $price;
+           }
+
+            public function show_price() {
+                echo $this->price;
+           }
+       }
+
+       class Animal {
+           private $name;
+           private $height;
+           private $weight;
+
+           public function __construct(string $name, int $height, int $weight) {
+                $this->name = $name;
+                $this->height = $height;
+                $this->weight = $weight;
+           }
+
+           public function show_height() {
+                echo $this->height;
+           }
+       }
+
+       $food = new Food("potato", 250);
+       $animal = new Animal("dog", 60, 5000);
+
+       print_r($food);
+       echo "<br>";
+       print_r($animal);
+       echo "<br>";
+
+       $food->show_price();
+       echo "<br>";
+       $animal->show_height();
+       echo "<br>";
+       ?>
+   </p>
+</body>
+
+</html>
